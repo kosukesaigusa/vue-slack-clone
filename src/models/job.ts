f3ee319d2a6cc8a4ae3d40c3ed3b2022f88c5bc1
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-05-15 12:54:46 +0900

    feat: implement job entity model

---
+++ src/models/job.ts
@@ -0,0 +1,55 @@
+import firebase from "firebase";
+
+export interface FirestoreJobData {
+  farmer_name: string;
+  description: string;
+  posted_at: firebase.firestore.Timestamp;
+  prefecture: string;
+  city: string;
+}
+
+export class Job {
+  farmerName = "";
+  description = "";
+  postedAt;
+  prefecture = "";
+  city = "";
+
+  constructor(
+    farmerName: string,
+    description: string,
+    postedAt: firebase.firestore.Timestamp,
+    prefecture: string,
+    city: string
+  ) {
+    this.farmerName = farmerName;
+    this.description = description;
+    this.postedAt = postedAt;
+    this.prefecture = prefecture;
+    this.city = city;
+  }
+}
+
+export const jobConverter = {
+  toFirestore: function (job: Job): firebase.firestore.DocumentData {
+    return {
+      farmer_name: job.farmerName,
+      description: job.description,
+      posted_at: job.postedAt,
+      prefecture: job.prefecture,
+      city: job.city,
+    };
+  },
+  fromFirestore: function (
+    snapshot: firebase.firestore.QueryDocumentSnapshot
+  ): Job {
+    const data = snapshot.data();
+    return new Job(
+      data.farmerName,
+      data.description,
+      data.postedAt,
+      data.prefecture,
+      data.city
+    );
+  },
+};
