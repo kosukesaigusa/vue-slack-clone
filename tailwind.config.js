004c2596fa5d0f0f00cc8491929d7161753f9fec
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-05-15 12:51:07 +0900

    chore: configure tailwind css and heroicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fill: (theme) => ({
+      red: theme("colors.red.500"),
+      green: theme("colors.green.500"),
+      blue: theme("colors.blue.500"),
+      gray: theme("colors.gray.500"),
+    }),
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
